48414cd9021d60824846a782efb6e23c3aa98a4e
skolodz1 <01168889@pw.edu.pl>
2022-05-17 13:06:34 +0200

    Plik nagłówkowey City.h pierwszy szkic

---
+++ City.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include "DirectConnection.h"
+class City
+{
+    private:
+    std::string name;
+    bool isTrainStation;
+    bool isBusStation;
+    std::vector<DirectConnection> dc_vec;
+    public:
+    City(std::string n, bool isTrain=false, bool isBus=false);
+    void add_conection(const& DirectConection dc);
+    std::string get_name() const;
+    std::vector<DirectConnection> get_dc_vec() const;
+    bool isBusStation() const;
+    bool isTrainStation() const;
+    void setTrainStation(bool);
+    void setBusStation(bool);
+};
