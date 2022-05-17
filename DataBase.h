33e53d9cb4ecfcda0362f242dde90a07fda2407a
skolodz1 <01168889@pw.edu.pl>
2022-05-17 13:48:11 +0200

    ...

---
+++ DataBase.h
@@ -0,0 +1,14 @@
+#include "DirectConection.h"
+#include "City.h"
+#include <vector>
+
+class DataBase
+{
+    private:
+    std::vector<Direct_Connection> connections;
+    std::vector<City> cities;
+    public:
+    // void load_file();
+    // Przeciążenie operatora []. Najlepiej kilka, np żeby dało się znaleźć miasto po nazwie
+
+}
